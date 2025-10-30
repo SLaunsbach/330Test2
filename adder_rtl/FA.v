27b3a13d263f7f80802b774feff243148b48c7d8
Stephen Launsbach <user.stephenlaunsbach@gmail.com>
2025-10-30 10:54:08 -0500

    feat(rca): added FA file to test

---
+++ adder_rtl/FA.v
@@ -0,0 +1,10 @@
+
+module rca(input wire A,
+        input wire B,
+        input wire CI,
+        output wire S,
+        output wire CO);
+
+    	xor iSUM (S, A, B, CI);
+	majority iCO(.V1(A), .V2(B), .V3(CI), .major(CO));
+endmodule
